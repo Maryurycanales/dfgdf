ac8d13016ed5de130d40cc0c19a0ab5ca9e7fc35
Maryurycanales <maryu_canales@hotmail.com>
2016-11-09 08:33:27 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+
+
+	int base=0;
+	int altura=0;
+		
+	cout<<"Ingrese la base: "<<base<<endl;
+	cin>>base;
+	
+	cout<<"Ingrese la altura: "<<altura<<endl;
+	cin>>altura;
+	
+	
+	cout<<"El area de un rectangulo es: "<<endl;
+	
+	cout<<"El area de un triangulo es: "<<endl;
+	
+	
+	
+	system("pause");
+	return 0;
+}
